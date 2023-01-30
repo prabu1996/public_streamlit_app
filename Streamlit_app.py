6ffa45969fb01ac0ae57cf1ccbc0f1e18b73f358
prabu1996 <93570188+prabu1996@users.noreply.github.com>
2023-01-30 15:21:22 +0530

    Update Streamlit_app.py
    1.connected pandas in github worksheet

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -3,3 +3,8 @@ import streamlit
 streamlit.title("Surya Kumar yadav")
 streamlit.header("Indian cricketer")
 streamlit.text("Surya is also called by 'Mr.360 of India'")
+
+import pandas as pd
+
+my_object=pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.my_object
