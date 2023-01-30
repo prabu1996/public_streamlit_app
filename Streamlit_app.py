350db5f3e7a7736be02b779910511da3f5ee69d4
prabu1996 <93570188+prabu1996@users.noreply.github.com>
2023-01-30 15:03:49 +0530

    Update Streamlit_app.py
    we added title, header and text

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
 
 streamlit.title("Surya Kumar yadav")
-
-streamlit.text("Surya MI")
+streamlit.header("Indian cricketer")
+streamlit.text("Surya is also called by "Mr.360 of India")
