2b6d79a84070d46e44cb17a6e10444a57378d705
prabu1996 <93570188+prabu1996@users.noreply.github.com>
2023-02-13 17:38:53 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('my parents new healthy diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('omega 3 and blueberry oatmeal')
+streamlit.text('kale ,spinach & rocket smoothie')
+streamlit.text('hard-boiled egg')
